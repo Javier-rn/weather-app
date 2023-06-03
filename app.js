952b6a51bbc4b17fe77c8adf3f5641d0ca38755a
Javier-rn <javier96rn@gmail.com>
2023-06-03 16:10:26 +0200

    Change http to https request in url

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function getDayOfWeek(formattedDate) {
 
 async function getWeather(place) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=b588725b22be4c8d8c492256232505&q=${place}&days=4&aqi=no&alerts=no`,
+    `https://api.weatherapi.com/v1/forecast.json?key=b588725b22be4c8d8c492256232505&q=${place}&days=4&aqi=no&alerts=no`,
     { mode: 'cors' }
   );
   const data = await response.json();
